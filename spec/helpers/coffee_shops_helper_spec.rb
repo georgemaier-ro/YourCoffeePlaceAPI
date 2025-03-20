18f8264b4b1164b7ad21ee0014ad98e5c26a1269
georgemaier-ro <maier.george91@gmail.com>
2025-03-20 11:37:04 +0200

    [YCPAPI-0008] Fix sort_by with strings bug

--- spec/helpers/coffee_shops_helper_spec.rb
+++ spec/helpers/coffee_shops_helper_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe CoffeeShopsHelper, type: :helper do
   let!(:longitude) { '-100.2567' }
   let!(:latitude_second) { '47.6' }
   let!(:longitude_second) { '-122.4' }
+  let!(:latitude_third) { '22.56' }
+  let!(:longitude_third) { '-120.567' }
 
   describe 'sort_coffee_shops_by_distance' do
     context 'when valid lat and lng' do
@@ -27,6 +29,12 @@ RSpec.describe CoffeeShopsHelper, type: :helper do
         sorted_table = sort_coffee_shops_by_distance(latitude_second, longitude_second)
         expect(sorted_table.present?).to be true
         expect(sorted_table).to eq([ [ "Starbucks Seattle2", "0.0645", "47.5869", "-122.3368" ], [ "Starbucks Seattle", "0.0861", "47.5809", "-122.3160" ], [ "Starbucks SF", "10.0793", "37.5209", "-122.3340" ] ])
+      end
+
+      it 'returns required locations with lat=22.56 and lng=-120.567' do
+        sorted_table = sort_coffee_shops_by_distance(latitude_third, longitude_third)
+        expect(sorted_table.present?).to be true
+        expect(sorted_table).to eq([ [ "Starbucks SF", "15.0649", "37.5209", "-122.3340" ], [ "Starbucks Seattle", "25.082", "47.5809", "-122.3160" ], [ "Starbucks Seattle2", "25.0894", "47.5869", "-122.3368" ] ])
       end
     end
 
